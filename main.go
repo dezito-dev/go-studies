96d20c55d6d9ac3d1126de9905f67d69554481e8
andre831 <andrelucianodossantosjunior@gmail.com>
2024-06-09 10:52:39 -0300

    feat: add structs

--- main.go
+++ main.go
@@ -2,8 +2,15 @@ package main
 
 import "fmt"
 
+/* ============== Aggregate type ===============*/
+/*
+ aggregate type is a type of data that groups together other values or variables. These values can be of the same type or different types. In Go, arrays, structs, and slices are examples of aggregate types.
+*/
+
 func main() {
 	fmt.Println("Studying Arrays in GoLang")
+
+	/* ============== Arrays ===============*/
 
 	var arr [5]int   //declaring a empty array int with 5 spaces   // array needs set the size and type, not can be changed
 	fmt.Println(arr) // [0 0 0 0 0]
@@ -19,4 +26,28 @@ func main() {
 
 	arraySizeAutoCalc := [...]string{"a", "b", "c"} // declaring an array with automatic size calc (need use "" for string)
 	fmt.Printf("%v\n", arraySizeAutoCalc)           // [a b c] // size 3
+
+	/* ============== Structs ===============*/
+	// structs are a composite data type to group together different data types
+
+	type Person struct {
+		name    string
+		age     int
+		weight  float64
+		hobbies []string // it's a slice
+	}
+
+	var p1 Person // declaring a variable of type Person
+
+	p1.name = "Andre" // assing values to the struct fields
+	p1.age = 21
+	p1.weight = 70.5
+	p1.hobbies = []string{"Play games", "Read books"}
+
+	fmt.Println(p1) // {Andre 21 70.5 [Play games Read books]}
+
+	// declaring and initializing a struct
+	p2 := Person{name: "John", age: 30, weight: 80.5, hobbies: []string{"Play games", "Read books"}}
+	fmt.Println(p2) // {John 30 80.5 [Play games Read books]}
+
 }
