b537f37cbd0c6e27b9eecf31abbe669885be5251
andre831 <andrelucianodossantosjunior@gmail.com>
2024-06-09 10:15:25 -0300

    feat: study basics data types

--- main.go
+++ main.go
@@ -4,4 +4,24 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	// using explicit declaration for variable
+	var name string = "André" // string type
+	fmt.Printf("Type: %T, Value: %v\n", name, name)
+
+	var age int = 25 // int type
+	fmt.Printf("Type: %T, Value: %v\n", age, age)
+
+	var isValid bool = false // boolean type
+	fmt.Printf("Type: %T, Value: %v\n", isValid, isValid)
+
+	var weight float64 = 80.1 // float64 type
+	fmt.Printf("Type: %T, Value: %v\n", weight, weight)
+
+	// using implicit declaration for variable
+	var city = "Floarianopolis" // string type
+	fmt.Printf("Type: %T, Value: %v\n", city, city)
+
+	year := 2024 // string type
+	fmt.Printf("Type: %T, Value: %v\n", year, year)
 }
