4335a9b48ad24c8ab2cbe7c1dfb3939274338335
andre831 <andrelucianodossantosjunior@gmail.com>
2024-06-09 10:33:42 -0300

    feat: declaring arrays

--- main.go
+++ main.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("Studying Arrays in GoLang")
+
+	var arr [5]int   //declaring a empty array int with 5 spaces   // array needs set the size and type, not can be changed
+	fmt.Println(arr) // [0 0 0 0 0]
+
+	arr[0] = 1       // assing a value for index 0
+	fmt.Println(arr) // [1 0 0 0 0]
+
+	arrayOfNumbers := [5]int{1, 2, 3, 4, 5} // declaring and initializing an array
+	fmt.Println(arrayOfNumbers)             // [1 2 3 4 5]
+
+	arrayPartilized := [5]int{1, 2}     // declaring an array partializing the size
+	fmt.Printf("%v\n", arrayPartilized) // [1 2 0 0 0]
+
+	arraySizeAutoCalc := [...]string{"a", "b", "c"} // declaring an array with automatic size calc (need use "" for string)
+	fmt.Printf("%v\n", arraySizeAutoCalc)           // [a b c] // size 3
 }
