5b3a1ac625e9e227ce6a1d625f915be98772112e
andre831 <andrelucianodossantosjunior@gmail.com>
2024-06-20 00:24:59 -0300

    training basic variables

--- main.go
+++ main.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	const name string = "André"
+	const age int = 10
+	const weight float64 = 55.5
+	var isCool = true
+
+	isCool = false
+
+	isBeautiful := true
+	isBeautiful = false
+
+	fmt.Println("My name is: ", name)
+	fmt.Println("My age: ", age)
+	fmt.Println("My weight: ", weight)
+	fmt.Println("Am I cool? ", isCool)
+	fmt.Println("Am I beautiful? ", isBeautiful)
 }
